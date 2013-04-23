0da648d1551ba501895c0db1a7a6059829b499af
Victor Coulon <victor@hull.io>
2013-04-23 17:27:52 +0200

    Relative assets

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@ require 'bootstrap-sass'
 
 
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = ""
 css_dir = "stylesheets"
 sass_dir = "sass"
 images_dir = "images"
@@ -13,7 +13,7 @@ javascripts_dir = "javascripts"
 # output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
