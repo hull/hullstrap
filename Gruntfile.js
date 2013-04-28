28425fb171c6abeb01ac61e51571968fe974a3eb
Jimmy Oliger <hi@jimmyoliger.com>
2013-04-28 14:29:02 +0200

    Add deploy task in Gruntfile
    https://trello.com/card/hull-bootstrap/50fc681eb497929412005a84/232

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,68 +1,133 @@
-/*global module:false*/
 module.exports = function(grunt) {
-  'use strict';
-
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-s3');
 
-  var SASSDIR = 'assets/sass',
-    CSSDIR = 'assets/css',
-    JSDIR = 'assets/js/',
-    IMAGESDIR = 'assets/images/',
-    DIRSOURCE = 'src/',
-    DIRLIBS = 'libs/',
-    DIRPLUGINS = 'plugins/',
-    DIRMODULES = 'modules/',
-    port = 3001;
+  function readOptionalJSON(file) {
+    return (grunt.file.exists(file)) ? grunt.file.readJSON(file) : {};
+  }
 
-  var gruntConfig = {
-    watch: {
-      compass: {
-        files: [
-          SASSDIR+'/**/*.{scss,sass}'
-        ],
-        tasks: 'compass:dev'
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    clean: ['dist/**'],
+
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'assets/sass/',
+          cssDir: 'assets/css/',
+          imagesDir: 'assets/images/',
+          relativeAssets: false
+        }
+      },
+      develop: {
+        sassDir: 'assets/sass/',
+        cssDir: 'assets/css/',
+        imagesDir: 'assets/images/',
+        relativeAssets: false,
+        outputStyle: 'expanded',
+        debugInfo: true,
+        force: true
       }
     },
+
+    cssmin: {
+      dist: {
+        files: {
+          'dist/<%= version %>/hullstrap.css': ['assets/css/*.css']
+        }
+      }
+    },
+
+    uglify: {
+      dist: {
+        files: {
+          'dist/<%= version %>/hullstrap.js': ['assets/js/*.js']
+        }
+      }
+    },
+
+    imagemin: {
+      dist: {
+        expand: true,
+        cwd: 'assets/images/',
+        src: ['*.{png,jpg,jpeg}'],
+        dest: 'dist/<%= version %>/images/'
+      }
+    },
+
+    watch: {
+      compass: {
+        files: ['assets/sass/**/*.{scss,sass}'],
+        tasks: 'compass'
+      }
+    },
+
     connect: {
       server: {
         options: {
-          port: port
+          port: 3001
         }
       }
     },
-    compass: {
-      dev: {
-        options: {
-          sassDir: SASSDIR,
-          cssDir: CSSDIR,
-          outputStyle: 'expanded',
-          noLineComments: false,
-          force: true,
-          debugInfo: true,
-          imagesDir: IMAGESDIR,
-          relativeAssets: true
+
+    aws: readOptionalJSON('aws.json'),
+
+    s3: {
+      options: {
+        key: '<%= aws.key %>',
+        secret: '<%= aws.secret %>',
+        bucket: '<%= aws.bucket %>',
+        access: 'public-read',
+        headers: {
+          'Cache-Control': 'public, max-age=<%= cache %>'
         }
       },
-      prod: {
-        options: {
-          sassDir: SASSDIR,
-                    cssDir: CSSDIR,
-          outputStyle: 'compressed',
-          noLineComments: true,
-          force: true,
-          debugInfo: false,
-          imagesDir: IMAGESDIR,
-          relativeAssets: false
-        }
+      dist: {
+        upload: [
+          {
+            src: 'dist/<%= version %>/**',
+            dest: '<%= target %>',
+            rel: 'dist/<%= version %>'
+          },
+          {
+            gzip: true,
+            src: 'dist/<%= version %>/**',
+            dest: '<%= target %>',
+            rel: 'dist/<%= version %>'
+          }
+        ]
       }
     }
-  };
-  grunt.initConfig(gruntConfig);
+  });
 
-  // default build task
-  grunt.registerTask('default', ['compass:dev', 'connect','watch']);
-  grunt.registerTask('build', ['compass:prod']);
+  grunt.registerTask('prepare', function() {
+    var done = this.async();
+    grunt.util.spawn({
+      cmd: 'git',
+      args: ['rev-parse', '--abbrev-ref', 'HEAD']
+    }, function(error, result) {
+      var branch = String(result).split('/').pop();
+      var isMaster = (branch === 'master');
+      var version = grunt.option('name') || (isMaster ? grunt.config.get('pkg.version') : branch);
 
+      grunt.config.set('version', version);
+      grunt.config.set('cache', (isMaster ? '29030400' : '0'));
+      grunt.config.set('target', grunt.option('target') || version);
+
+      done();
+    });
+  });
+
+  grunt.registerTask('develop', ['compass:develop', 'connect', 'watch']);
+  grunt.registerTask('dist', ['prepare', 'clean', 'compass:dist', 'cssmin' , 'uglify', 'imagemin']);
+  grunt.registerTask('deploy', ['dist', 's3']);
+
+  grunt.registerTask('default', 'develop');
 };
