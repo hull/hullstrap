474b8259645a02f98cf634937563bd9eff8ae913
Victor Coulon <victor@hull.io>
2013-04-29 11:08:40 +0200

    Change uglify order

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,7 +48,21 @@ module.exports = function(grunt) {
     uglify: {
       dist: {
         files: {
-          'dist/<%= version %>/hullstrap.js': ['assets/js/*.js']
+          'dist/<%= version %>/hullstrap.js':
+            [
+              'assets/js/bootstrap-transition.js',
+              'assets/js/bootstrap-alert.js',
+              'assets/js/bootstrap-modal.js',
+              'assets/js/bootstrap-dropdown.js',
+              'assets/js/bootstrap-scrollspy.js',
+              'assets/js/bootstrap-tab.js',
+              'assets/js/bootstrap-tooltip.js',
+              'assets/js/bootstrap-popover.js',
+              'assets/js/bootstrap-button.js',
+              'assets/js/bootstrap-collapse.js',
+              'assets/js/bootstrap-carousel.js',
+              'assets/js/bootstrap-typeahead.js'
+            ]
         }
       }
     },
