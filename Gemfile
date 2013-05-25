7198851aab183cc03183335925396f6ca10ee70d
Romain Dardour <romain@hull.io>
2013-05-25 16:51:59 -0600

    Move to Middleman

---
+++ Gemfile
@@ -0,0 +1,28 @@
+source 'http://rubygems.org'
+
+gem 'rack-contrib'
+gem 'rack-environment'
+gem 'activesupport'
+
+gem 'slim'
+
+gem 'animation' # css animation plugin
+gem 'compass', :git => 'git://github.com/chriseppstein/compass.git', :tag => 'v0.13.alpha.4'
+gem 'sass-getunicode'
+gem 'bootstrap-sass', '~> 2.3.1.0'
+
+gem 'libv8', '~> 3.11.8'
+gem 'execjs'
+gem 'oj'        # faster JSON
+
+gem 'middleman'
+gem 'middleman-sprockets'
+gem 'middleman-gh-pages'
+gem 'middleman-smusher'
+gem 'middleman-livereload'
+
+group :development do
+  gem 'heroku'
+  gem 'shotgun'
+  gem 'fontcustom', :git => 'git@github.com:hull/fontcustom.git', :branch => 'big-refactor'
+end
---
+++ config.rb
@@ -0,0 +1,76 @@
+###
+# Compass
+###
+
+# Susy grids in Compass
+# First: gem install susy
+# require 'susy'
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy (fake) files
+# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
+#   @which_fake_page = "Rendering a fake page with a variable"
+# end
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
+
+set :build_dir, 'tmp'
+
+activate :livereload
+
+page "*", :layout => :layout
+page "admin/*", :layout => :admin
+
+require 'lib/retina.rb'
+require 'lib/random-color.rb'
+Sass::Script::Number.precision = 20
+
+# Build-specific configuration
+configure :build do
+
+  activate :minify_css
+  activate :minify_javascript
+  activate :cache_buster
+  activate :gzip
+
+  set :logging, true
+end
