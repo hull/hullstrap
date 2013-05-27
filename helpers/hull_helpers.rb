7f67498e772aa86ad69d056a15bbc4032da1a65f
Romain Dardour <romain@hull.io>
2013-05-27 02:22:25 -0600

    Update icons

--- helpers/hull_helpers.rb
+++ helpers/hull_helpers.rb
@@ -2,7 +2,7 @@ module HullHelpers
 
   def icon_list
     return @icons unless @icons.nil?
-    icon_list = Dir.entries(File.dirname(__FILE__) + '/../source/images/icons').select {|f| File.file?(f)}
+    icon_list = Dir.entries(File.dirname(__FILE__) + '/../source/images/icons')
     @icons ||= icon_list.map {|i| i.gsub('.svg','')}
   end
 end
